fe982ac9d61949d0c8d574caa25edf8b02fba646
Thorsten Lorenz <thlorenz@gmx.de>
2023-08-20 22:23:02 +0700

    types: don't depend on node fetch type

--- types/fetch.d.ts
+++
@@ -1,7 +0,0 @@
-import type * as unidici from 'undici'
-
-declare global {
-  var fetch: typeof unidici.fetch
-  class Request extends unidici.Request {}
-  class Response extends unidici.Response {}
-}
