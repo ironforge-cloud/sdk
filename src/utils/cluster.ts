bdd03e061d1a556e853437a6b316ed3f49937fb3
Thorsten Lorenz <thlorenz@gmx.de>
2023-08-22 14:30:49 +0700

    docs: for cluster

--- src/utils/cluster.ts
+++ src/utils/cluster.ts
@@ -1,10 +1,17 @@
+/** mainnet cluster */
 export const MAINNET = 'mainnet'
+/** devnet cluster */
 export const DEVNET = 'devnet'
+/** testnet cluster */
 export const TESTNET = 'testnet'
 
+/** All available clusters. */
 export const Clusters = [MAINNET, DEVNET, TESTNET] as const
+
+/** Cluster */
 export type Cluster = (typeof Clusters)[number]
 
+/** Checks if the given cluster is valid. */
 export function isValidCluster(cluster?: string): cluster is Cluster {
   return cluster != null && Clusters.includes(cluster as Cluster)
 }
